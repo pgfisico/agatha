c03e1295eccac5ace654ee0696afa71ea33977ef
pgfisico <pgfisico@users.noreply.github.com>
2018-02-17 12:48:43 -0700

    Invert left Drivetrain CIMs

--- src/Subsystems/DriveTrain.cpp
+++ src/Subsystems/DriveTrain.cpp
@@ -1,13 +1,16 @@
-#include <Subsystems/DriveTrain.h>
-#include "../RobotMap.h"
+#include "Subsystems/DriveTrain.h"
+
+
 #include "Commands/DriveWithJoystick.h"
+#include "RobotMap.h"
 
-DriveTrain::DriveTrain() : frc::Subsystem("ExampleSubsystem") {
-
-
+DriveTrain::DriveTrain() : frc::Subsystem("ExampleSubsystem")
+{
+	leftCIMs.SetInverted(true);
 }
 
-void DriveTrain::InitDefaultCommand() {
+void DriveTrain::InitDefaultCommand()
+{
 	// Set the default command for a subsystem here.
 	SetDefaultCommand(new DriveWithJoystick());
 }
@@ -15,8 +18,8 @@ void DriveTrain::InitDefaultCommand() {
 // Put methods for controlling this subsystem
 // here. Call these from Commands.
 
-void DriveTrain::ArcadeDrive(double leftAxis, double rightAxis){
-
+void DriveTrain::ArcadeDrive(double leftAxis, double rightAxis)
+{
 	robotDrive.ArcadeDrive(leftAxis, rightAxis);
 }
 
